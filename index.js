f8bf6ebc71a30915a9d6b81f5608aeb14ab28aa4
Pulzze92 <89998310392@mail.ru>
2021-06-16 17:31:47 +0300

    feat: finish script

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 let $start = document.querySelector('#start');
 let $game = document.querySelector('#game');
 let $time = document.querySelector('#time');
+let $timeHeader = document.querySelector('#time-header');
+let $resultHeader = document.querySelector('#result-header');
+let $gameTime = document.querySelector('#game-time');
+
+let colors = ['red', 'blue', 'green', 'yellow', 'orange', 'brown', 'gold', 'purple', 'pink', 'black', 'gray'];
 
 let score = 0;
 let isGameStarted = false;
 
 $start.addEventListener('click', startGame);
 $game.addEventListener('click', handleBoxClick);
+$gameTime.addEventListener('input', setGameTime);
+
+function show($el) {
+    $el.classList.remove('hide');
+}
+
+function hide($el) {
+    $el.classList.add('hide');
+}
 
 function startGame() {
+    score = 0;
+    setGameTime();
+    $gameTime.removeAttribute('disabled');
+
     isGameStarted = true;
     $game.style.backgroundColor = '#fff';
-    $start.classList.add('hide');
+    hide($start);
 
     let interval = setInterval(function () {
         let time = parseFloat($time.textContent);
@@ -25,8 +43,25 @@ function startGame() {
     renderBox();
 }
 
+function setGameTime() {
+    let time = +$gameTime.value;
+    $time.textContent = time.toFixed(1);
+    show($timeHeader);
+    hide($resultHeader);
+}
+
+function setGameScore() {
+    result.textContent = score.toString();
+}
+
 function endGame() {
     isGameStarted = false;
+    setGameScore();
+    show($start);
+    $game.innerHTML = '';
+    $game.style.backgroundColor = '#ccc';
+    hide($timeHeader);
+    show($resultHeader);
 }
 
 function handleBoxClick(e) {
@@ -46,10 +81,11 @@ function renderBox() {
     let gameSize = $game.getBoundingClientRect();
     let maxTop = gameSize.height - boxSize;
     let maxLength = gameSize.width - boxSize;
+    let randomColorIndex = getRandom(0, colors.length);
 
     box.style.height = box.style.width = boxSize + 'px';
     box.style.position = 'absolute';
-    box.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    box.style.backgroundColor = colors[randomColorIndex];
     box.style.top = getRandom(0, maxTop) + 'px';
     box.style.left = getRandom(0, maxLength) + 'px';
     box.style.cursor = 'pointer';
