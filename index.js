115e003c227fa8e66f0761fbab52ac6895c2ae02
Pulzze92 <89998310392@mail.ru>
2021-06-16 13:19:22 +0300

    feat: add square

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let $start = document.querySelector('#start');
 let $game = document.querySelector('#game');
 
+let score = 0;
+
 $start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
 
 function startGame() {
     $game.style.backgroundColor = '#fff';
@@ -9,6 +12,23 @@ function startGame() {
     renderBox();
 }
 
+function handleBoxClick(e) {
+    if (event.target.dataset) {
+        renderBox();
+        score++;
+    }
+}
+
 function renderBox() {
-    
+    $game.innerHTML = '';
+    let box = document.createElement('div');
+    box.style.height = box.style.width = '50px';
+    box.style.position = 'absolute';
+    box.style.backgroundColor = '#000';
+    box.style.top = '50px';
+    box.style.left = '50px';
+    box.style.cursor = 'pointer';
+    box.setAttribute('data-box', 'true');
+
+    $game.insertAdjacentElement('afterbegin', box)
 }
